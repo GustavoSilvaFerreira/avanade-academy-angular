db735e1de1b2c981d9e82cb4e8e50f74a886207d
GustavoSilvaFerreira <guto7626@hotmail.com>
2019-07-04 19:11:42 -0300

    aula 3

--- src/app/lista-todos/lista-todos.component.ts
+++ src/app/lista-todos/lista-todos.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TodoService } from '../todo.service';
 import { Todo } from 'src/typings/todo';
-
-
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-lista-todos',
@@ -11,15 +10,11 @@ import { Todo } from 'src/typings/todo';
 })
 export class ListaTodosComponent implements OnInit {
 
-  todos: Todo[];
+  todos$: Observable<Todo[]>;
 
   constructor(public todoService: TodoService) { }
 
   ngOnInit() {
-    this.todoService.getTodos()
-      .subscribe(value => {
-        this.todos = value;
-    });
-  }
-
+    this.todos$ = this.todoService.getTodos();
+  };
 }
