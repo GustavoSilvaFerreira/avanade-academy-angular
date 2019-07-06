00e78fbe0f882b89191eaf8d6d87376d58541219
GustavoSilvaFerreira <guto7626@hotmail.com>
2019-07-06 11:12:55 -0300

    aula 4

--- src/app/cep/cep.service.ts
+++ src/app/cep/cep.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Cep } from 'src/typings/cep';
 
 
 @Injectable({
@@ -10,6 +11,6 @@ export class CepService {
   constructor(private httpClient: HttpClient) { }
 
   getCep(cep: string) {
-    return this.httpClient.get('http://viacep.com.br/ws/' + cep + '/json/');
+    return this.httpClient.get<Cep>('http://viacep.com.br/ws/' + cep + '/json/');
   }
 }
